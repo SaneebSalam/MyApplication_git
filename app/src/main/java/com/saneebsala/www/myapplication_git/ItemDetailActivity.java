95cffb68b663485c8455f93e053cfd147fa0693a
saneeb <saneebsalam@gmail.com>
2017-10-02 10:59:11 +0300

    update

--- app/src/main/java/com/saneebsala/www/myapplication_git/ItemDetailActivity.java
+++ app/src/main/java/com/saneebsala/www/myapplication_git/ItemDetailActivity.java
@@ -25,7 +25,7 @@ public class ItemDetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
 
-        System.out.println("Saneeb salam");
+        System.out.println("Saneeb Salam");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
